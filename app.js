98382e59f09a0a7607228449ca5af4b8748abcd0
ƖῳƖც ɠąɱıŋɠ ɧųც ơʄʄıƈıąƖ <lwlb.gaminghub.official@gmail.com>
2026-05-18 20:08:57 +0300

    Add music tracks data structure
    Added an array of music tracks with details including title, artist, cover image, links, and optional descriptions.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+[
+  {
+    "id": "track-001",
+    "title": "اسم الأغنية 1",
+    "artist": "اسم الفنان",
+    "cover": "https://picsum.photos/seed/cover1/600/600",
+    "links": {
+      "youtube": "https://www.youtube.com/watch?v=XXXXXXXXXXX",
+      "spotify": "https://open.spotify.com/track/XXXXXXXXXXX",
+      "soundcloud": "https://soundcloud.com/XXXXXXXXXXX"
+    },
+    "description": "وصف بسيط للأغنية (اختياري)."
+  },
+  {
+    "id": "track-002",
+    "title": "اسم الأغنية 2",
+    "artist": "اسم الفنان",
+    "cover": "https://picsum.photos/seed/cover2/600/600",
+    "links": {
+      "youtube": "https://www.youtube.com/watch?v=YYYYYYYYYYY",
+      "spotify": "https://open.spotify.com/track/YYYYYYYYYYY"
+    },
+    "description": "وصف بسيط (اختياري)."
+  }
+]
 
